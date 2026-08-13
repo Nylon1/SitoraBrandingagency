ede937d2ad42ccfe2941b04997f13b79c7021c36
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2026-08-13 10:46:19 +0100

    Add management operating review route

---
+++ app/tools/dental-control/management/page.tsx
@@ -0,0 +1,5 @@
+import { ManagementOperatingReview } from "@/components/dental-control/ManagementOperatingReview";
+
+export default function ManagementPage() {
+  return <ManagementOperatingReview />;
+}
