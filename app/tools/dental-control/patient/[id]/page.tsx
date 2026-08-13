6374ed6474662e0e5d814c95496f1f2e11ac84d4
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2026-08-13 09:56:00 +0100

    Add patient opportunity story route

---
+++ app/tools/dental-control/patient/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { PatientOpportunityStory } from "@/components/dental-control/PatientOpportunityStory";
+
+export default function PatientOpportunityPage() {
+  return <PatientOpportunityStory />;
+}
